1fc7e234ff4d594c5dbc8a597d0aa62f123ffa4b
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-03-01 22:27:04 -0300

    Exercicio 1 e 2

---
+++ Lista/src/Algoritmos/Exercicio1.java
@@ -0,0 +1,16 @@
+package Algoritmos;
+import java.util.Scanner;
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double salario, salarioAcrescido;
+
+        System.out.print("Informe o seu salário: ");
+        salario = entrada.nextDouble();
+
+        salarioAcrescido = salario + (salario * 0.25);
+
+        System.out.println("O novo salário será: R$" + salarioAcrescido);
+
+    }
+}
---
+++ Lista/src/Algoritmos/Exercicio2.java
@@ -0,0 +1,19 @@
+package Algoritmos;
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double salario, percentual, salarioAcrescido;
+
+        System.out.print("Informe o seu salário: ");
+        salario = entrada.nextDouble();
+
+        System.out.print("Informe a porcentagem que deverá ser acrescida: ");
+        percentual = entrada.nextDouble();
+
+        salarioAcrescido = salario + (salario * (percentual / 100));
+
+        System.out.println("O novo salário será: R$" + salarioAcrescido);
+    }
+}
