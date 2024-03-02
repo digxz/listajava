14a3987d90bca38daa5013e553dc2b0d5403d797
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-03-01 22:37:55 -0300

    Exercicio 1 e 2

--- Lista/src/Algoritmos/Exercicio1.java
+++ Lista/src/Algoritmos/Exercicio1.java
@@ -1,5 +1,6 @@
 package Algoritmos;
 import java.util.Scanner;
+
 public class Exercicio1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
---
+++ Lista/src/Algoritmos/Exercicio3.java
@@ -0,0 +1,24 @@
+package Algoritmos;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double anoDeNascimento, anoAtual, idadeAnos, idadeMeses, idadeDias;
+
+        System.out.print("Informe o ano que você nasceu: ");
+        anoDeNascimento = entrada.nextDouble();
+
+        System.out.print("Informe o ano atual: ");
+        anoAtual = entrada.nextDouble();
+
+        idadeAnos = anoAtual - anoDeNascimento;
+        idadeMeses = idadeAnos * 12;
+        idadeDias = idadeMeses * 30.4167;
+
+        System.out.println("Sua idade em anos é: " + idadeAnos);
+        System.out.println("Sua idade em meses é: " + idadeMeses);
+        System.out.println("Sua idade em dias é: " + idadeDias);
+
+    }
+}
