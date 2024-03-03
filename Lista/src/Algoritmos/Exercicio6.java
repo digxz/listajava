cf7709de0df0c5cbd1eb0c746cc5f0f7e0cbde4a
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-03-03 12:02:03 -0300

    Exercício 6

---
+++ Lista/src/Algoritmos/Exercicio6.java
@@ -0,0 +1,23 @@
+package Algoritmos;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double comprimento, largura, altura, volume;
+
+        System.out.print("Digite o comprimento da caixa: ");
+        comprimento = entrada.nextDouble();
+
+        System.out.print("Digite a largura da caixa: ");
+        largura = entrada.nextDouble();
+
+        System.out.print("Digite a altura da caixa: ");
+        altura = entrada.nextDouble();
+
+        volume = comprimento * largura * altura;
+
+        System.out.println("O volume da caixa é: " + volume);
+    }
+}
