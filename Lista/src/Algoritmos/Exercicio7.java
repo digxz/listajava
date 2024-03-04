804697e1002331b04834748589f841a71a5e14da
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-03-03 21:53:33 -0300

    Exercício 7

---
+++ Lista/src/Algoritmos/Exercicio7.java
@@ -0,0 +1,21 @@
+package Algoritmos;
+
+import java.util.Scanner;
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double varA, varB, diferenca, quadrado;
+
+        System.out.print("Digite a variável A: ");
+        varA = entrada.nextDouble();
+
+        System.out.print("Digite a variável B: ");
+        varB = entrada.nextDouble();
+
+        diferenca = varA - varB;
+        quadrado = diferenca * diferenca;
+
+        System.out.println("O quadrado da diferença entre as variáveis resulta em: " + quadrado);
+
+    }
+}
