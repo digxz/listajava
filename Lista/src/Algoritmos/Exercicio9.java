c819ecfc490c57be6d4acce4b086f5da7f25433f
digxz <diegocastelliniramirez.dcr@gmail.com>
2024-03-03 22:08:25 -0300

    Exercício 9

---
+++ Lista/src/Algoritmos/Exercicio9.java
@@ -0,0 +1,23 @@
+package Algoritmos;
+
+import java.util.Scanner;
+public class Exercicio9 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double varA, varB, varC, soma, quadrado;
+
+        System.out.print("Digite a variável A: ");
+        varA = entrada.nextDouble();
+
+        System.out.print("Digite a variável B: ");
+        varB = entrada.nextDouble();
+
+        System.out.print("Digite a variável C: ");
+        varC = entrada.nextDouble();
+
+        soma = varA + varB + varC;
+        quadrado = soma * soma;
+
+        System.out.println("O quadrado da soma entre as variáveis resulta em: " + quadrado);
+    }
+}
